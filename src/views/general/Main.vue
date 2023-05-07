948e05c23a4b348dfdf3c12e36848a64e963ab56
svr-systems <samuel@svrmexico.com>
2023-05-06 23:31:35 -0600

    Ajuste para navbar

--- src/views/general/Main.vue
+++ src/views/general/Main.vue
@@ -34,38 +34,220 @@
       </v-list>
     </v-navigation-drawer>
 
-    <v-app-bar app color="black" dense dark>
-      <v-app-bar-nav-icon class="white--text" @click="drawer = true" />
-      <v-spacer />
-
-      <div class="d-flex align-center">
+    <v-card class="overflow-hidden" tile>
+      <v-app-bar
+        color="black"
+        absolute
+        dense
+        dark
+        inverted-scroll
+        scroll-target="#main"
+      >
+        <v-app-bar-nav-icon class="white--text" @click="drawer = true" />
         <v-img
+          class="mx-2"
           :src="require('@/assets/logo.png')"
-          max-height="40px"
-          class="my-1"
+          max-height="40"
+          max-width="60"
           contain
         />
-      </div>
-      <v-spacer />
+        <v-spacer />
+        <v-tooltip v-for="(social_btn, i) in social_btns" :key="i" bottom>
+          <template v-slot:activator="{ on }">
+            <v-btn
+              v-on="on"
+              class="mr-1"
+              small
+              icon
+              :href="social_btn.url"
+              target="_blank"
+            >
+              <v-img
+                :src="require(`@/assets/img/social/${social_btn.icon}`)"
+                max-height="28"
+                max-width="28"
+              />
+            </v-btn>
+          </template>
+          <span v-text="social_btn.name" />
+        </v-tooltip>
+      </v-app-bar>
+      <v-sheet id="main" class="overflow-y-auto full-height">
+        <v-container fluid>
+          <v-row>
+            <v-col
+              cols="12"
+              class="full-height full-bg"
+              :style="`background-image: url('${require('@/assets/img/bg/1.jpg')}');`"
+            >
+              <v-row class="white--text mt-5" align="center">
+                <v-col cols="12" sm="12" md="5" class="text-right">
+                  <span class="mx-5 text-h5" v-text="'Nosotros'" />
+                  <span class="mx-5 text-h5" v-text="'Fechas'" />
+                </v-col>
+                <v-col cols="12" sm="12" md="2" class="text-center">
+                  <img :src="require('@/assets/logo.png')" height="120px" />
+                </v-col>
+                <v-col cols="12" sm="12" md="5">
+                  <span class="mx-5 text-h5" v-text="'StagePlot'" />
+                  <span class="mx-5 text-h5" v-text="'Contacto'" />
+                </v-col>
+                <v-col cols="12" class="text-center">
+                  <v-tooltip
+                    v-for="(social_btn, i) in social_btns"
+                    :key="i"
+                    bottom
+                  >
+                    <template v-slot:activator="{ on }">
+                      <v-btn
+                        v-on="on"
+                        class="mx-1"
+                        small
+                        icon
+                        :href="social_btn.url"
+                        target="_blank"
+                      >
+                        <v-img
+                          :src="
+                            require(`@/assets/img/social/${social_btn.icon}`)
+                          "
+                          max-height="25"
+                          max-width="25"
+                        />
+                      </v-btn>
+                    </template>
+                    <span v-text="social_btn.name" />
+                  </v-tooltip>
+                </v-col>
+              </v-row>
+            </v-col>
+            <v-col cols="12">
+              text
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              fin test
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-sheet>
+    </v-card>
+    <v-footer app color="black" dark padless>
+      <v-row dense>
+        <v-col cols="12" class="text-center">
+          <small
+            v-text="`Copyright © ${new Date().getFullYear()}`"
+            class="pr-1"
+          />
+        </v-col>
+      </v-row>
+    </v-footer>
 
-      <v-btn icon>
-        <v-icon>mdi-heart</v-icon>
-      </v-btn>
-      <v-btn icon>
-        <v-icon>mdi-heart</v-icon>
-      </v-btn>
-      <v-btn icon>
-        <v-icon>mdi-heart</v-icon>
-      </v-btn>
-      <v-btn icon>
-        <v-icon>mdi-heart</v-icon>
-      </v-btn>
-      <v-btn icon>
-        <v-icon>mdi-heart</v-icon>
-      </v-btn>
-      <v-btn icon>
-        <v-icon>mdi-heart</v-icon>
-      </v-btn>
+    <!-- <v-app-bar app color="black" dense dark>
+      <v-app-bar-nav-icon class="white--text" @click="drawer = true" />
+      <v-img
+        class="mx-2"
+        :src="require('@/assets/logo.png')"
+        max-height="40"
+        max-width="60"
+        contain
+      />
+      <v-spacer />
+      <v-tooltip v-for="(social_btn, i) in social_btns" :key="i" bottom>
+        <template v-slot:activator="{ on }">
+          <v-btn
+            v-on="on"
+            class="mr-1"
+            small
+            icon
+            :href="social_btn.url"
+            target="_blank"
+          >
+            <v-img
+              :src="require(`@/assets/img/social/${social_btn.icon}`)"
+              max-height="28"
+              max-width="28"
+            />
+          </v-btn>
+        </template>
+        <span v-text="social_btn.name" />
+      </v-tooltip>
     </v-app-bar>
 
     <v-main>
@@ -74,7 +256,7 @@
           <v-col
             cols="12"
             class="full-height full-bg"
-            :style="`background-image: url('${require('@/assets/img/bg1.jpg')}');`"
+            :style="`background-image: url('${require('@/assets/img/bg/1.jpg')}');`"
           >
             HOLA
           </v-col>
@@ -174,7 +356,7 @@
           />
         </v-col>
       </v-row>
-    </v-footer>
+    </v-footer> -->
   </v-app>
 </template>
 
@@ -190,6 +372,38 @@ export default {
           icon: "mdi-home",
           link: "main",
           visible: true,
+        },
+      ],
+      social_btns: [
+        {
+          name: "Instagram",
+          icon: "ig.svg",
+          url: "https://www.instagram.com/licenciadopunk/",
+        },
+        {
+          name: "Youtube",
+          icon: "yt.svg",
+          url: "https://www.youtube.com/channel/UCCQb24WxAC2wpMVrMHgJbeQ",
+        },
+        {
+          name: "TikTok",
+          icon: "tt.svg",
+          url: "https://www.tiktok.com/@licenciadopunk",
+        },
+        {
+          name: "Facebook",
+          icon: "fb.svg",
+          url: "https://www.facebook.com/Licenciado-Punk-101402515910552",
+        },
+        {
+          name: "Twitter",
+          icon: "tw.svg",
+          url: "https://twitter.com/LicenciadoPunk",
+        },
+        {
+          name: "WhatsApp",
+          icon: "wa.svg",
+          url: "https://wa.me/524611292699?text=Hola,%20somos%20Licenciado%20Punk!!!,%20¿qué%20podemos%20hacer%20por%20ti?",
         },
       ],
     };
