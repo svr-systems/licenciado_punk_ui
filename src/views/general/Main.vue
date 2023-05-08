ef32d29fcb0150af56fe14830411e802fdb84abe
svr-systems <samuel@svrmexico.com>
2023-05-07 21:04:56 -0600

    Integracion de modulos

--- src/views/general/Main.vue
+++ src/views/general/Main.vue
@@ -57,15 +57,15 @@
             <v-btn
               v-on="on"
               class="mr-1"
-              small
+              x-small
               icon
               :href="social_btn.url"
               target="_blank"
             >
               <v-img
                 :src="require(`@/assets/img/social/${social_btn.icon}`)"
-                max-height="28"
-                max-width="28"
+                max-height="24"
+                max-width="24"
               />
             </v-btn>
           </template>
@@ -80,32 +80,42 @@
               class="full-height full-bg"
               :style="`background-image: url('${require('@/assets/img/bg/1.jpg')}');`"
             >
-              <v-row class="white--text mt-5" align="center">
+              <v-row class="white--text" align="center">
+                <v-col cols="12" class="pt-8" />
                 <v-col
-                  v-if="!isMobile()"
+                  v-if="!screenMobile"
                   cols="12"
                   sm="12"
                   md="5"
                   class="text-right"
                 >
-                  <span class="mx-5 text-h5" v-text="'Nosotros'" />
-                  <span class="mx-5 text-h5" v-text="'Fechas'" />
+                  <span class="mx-5 c-title pointer" v-text="'Nosotros'" />
+                  <span class="mx-5 c-title pointer" v-text="'Shows'" />
                 </v-col>
                 <v-col cols="12" sm="12" md="2" class="text-center">
-                  <img :src="require('@/assets/logo.png')" height="120px" />
+                  <img
+                    :src="require('@/assets/logo.png')"
+                    :width="
+                      (!screenMobile
+                        ? 90
+                        : screenWidth > screenHeight
+                        ? 18
+                        : 70) + '%'
+                    "
+                  />
                 </v-col>
-                <v-col v-if="!isMobile()" cols="12" sm="12" md="5">
-                  <span class="mx-5 text-h5" v-text="'StagePlot'" />
-                  <span class="mx-5 text-h5" v-text="'Contacto'" />
+                <v-col v-if="!screenMobile" cols="12" sm="12" md="5">
+                  <span class="mx-5 c-title pointer" v-text="'StagePlot'" />
+                  <span class="mx-5 c-title pointer" v-text="'Contacto'" />
                 </v-col>
-                <v-col v-if="isMobile()" cols="12" class="text-center">
-                  <span class="text-h6" v-text="'Nosotros'" />
+                <v-col v-if="screenMobile" cols="12" class="text-center pt-0">
+                  <span class="mx-5 c-title pointer" v-text="'Nosotros'" />
                   <br />
-                  <span class="text-h6" v-text="'Fechas'" />
+                  <span class="mx-5 c-title pointer" v-text="'Shows'" />
                   <br />
-                  <span class="text-h6" v-text="'StagePlot'" />
+                  <span class="mx-5 c-title pointer" v-text="'StagePlot'" />
                   <br />
-                  <span class="text-h6" v-text="'Contacto'" />
+                  <span class="mx-5 c-title pointer" v-text="'Contacto'" />
                 </v-col>
                 <v-col cols="12" class="text-center">
                   <v-tooltip
@@ -136,88 +146,91 @@
                 </v-col>
               </v-row>
             </v-col>
-            <v-col cols="12">
-              text
-              <br />
-              <br />
-              <br />
-              <br />
-              <br />
-              <br />
-              <br />
-              <br />
-              <br />
-              <br />
-              <br />
-              <br />
-              <br />
-              <br />
-              <br />
-              <br />
-              <br />
-              <br />
-              <br />
-              <br />
-              <br />
-              <br />
-              <br />
-              <br />
-              <br />
-              <br />
-              <br />
-              <br />
-              <br />
-              <br />
-              <br />
-              <br />
-              <br />
-              <br />
-              <br />
-              <br />
-              <br />
-              <br />
-              <br />
-              <br />
-              <br />
-              <br />
-              <br />
-              <br />
-              <br />
-              <br />
-              <br />
-              <br />
-              <br />
-              <br />
-              <br />
-              <br />
-              <br />
-              <br />
-              <br />
-              <br />
-              <br />
-              <br />
-              <br />
-              <br />
-              <br />
-              <br />
-              <br />
-              <br />
-              <br />
-              <br />
-              <br />
-              <br />
-              <br />
-              <br />
-              <br />
-              <br />
-              <br />
-              <br />
-              <br />
-              <br />
-              <br />
-              <br />
-              <br />
-              fin test
+          </v-row>
+
+          <v-row class="py-5 my-5">
+            <v-col cols="12" class="text-center">
+              <h1>Nosotros</h1>
+            </v-col>
+            <v-col cols="12" sm="12" md="4" class="px-5">
+              <p class="text-justify">
+                Licenciado Punk es una banda formada en el año 2021, originaria
+                del estado de Guanajuato México, la cual experimenta con
+                diferentes géneros musicales tales como el Rock Alternativo,
+                Grunge, Noise Rock y Rock Punk.
+              </p>
+            </v-col>
+            <v-col cols="12" sm="12" md="4" class="px-5">
+              <p class="text-justify">
+                Su música busca tener la libertad de dejarlos hacer lo que les
+                guste, siempre con esa chispa de mucha energía para entregarle
+                al público una experiencia que deje marca para siempre; Así como
+                con su show dinámico y lleno de sorpresas, siendo diferente en
+                cada lugar al que van.
+              </p>
+            </v-col>
+            <v-col cols="12" sm="12" md="4" class="px-5">
+              <p class="text-justify">
+                Actualmente se publicó el primer single titulado “Mezcal” que
+                tiende a funcionar como el Demo del resto de obras compuestas
+                por la banda que pretenden publicarse en las plataformas
+                digitales a día de hoy.
+              </p>
+            </v-col>
+          </v-row>
+
+          <v-row class="py-5 my-5">
+            <v-col
+              v-for="(member, i) in members"
+              :key="i"
+              cols="12"
+              sm="12"
+              md="4"
+              :style="`background-image: url('${require(`@/assets/${member.url}`)}');`"
+              class="member member-img"
+              v-if="!screenMobile || (screenMobile && member.mobile)"
+            >
+              <div
+                v-if="member.name"
+                class="member-text text-center white--text mx-5"
+              >
+                <span class="member-title" v-text="member.name" />
+                <br />
+                <span class="member-subtitle" v-text="member.instrument" />
+              </div>
+            </v-col>
+          </v-row>
+
+          <v-row justify="center" class="py-5 my-5">
+            <v-col cols="12" class="text-center">
+              <h1>Shows</h1>
+            </v-col>
+            <v-col
+              v-for="(show, i) in shows"
+              :key="i"
+              cols="12"
+              sm="12"
+              md="3"
+              class="px-5 text-center pb-5"
+            >
+              <p>
+                {{ show.name }}
+                <br />
+                <small> {{ show.date }} | {{ show.hour }} hrs. </small>
+              </p>
+              <v-img max-height="400" :src="require(`@/assets/${show.url}`)" />
+            </v-col>
+          </v-row>
+
+          <v-row
+            class="text-center white--text py-5 mt-5"
+            style="background-color: #111"
+          >
+            <v-col cols="12" sm="12" md="6">
+              <h1>StagePlot</h1>
+            </v-col>
+            <v-col cols="12" sm="12" md="6">
+              <h1>Contacto</h1>
             </v-col>
           </v-row>
         </v-container>
@@ -233,145 +246,6 @@
         </v-col>
       </v-row>
     </v-footer>
-
-    <!-- <v-app-bar app color="black" dense dark>
-      <v-app-bar-nav-icon class="white--text" @click="drawer = true" />
-      <v-img
-        class="mx-2"
-        :src="require('@/assets/logo.png')"
-        max-height="40"
-        max-width="60"
-        contain
-      />
-      <v-spacer />
-      <v-tooltip v-for="(social_btn, i) in social_btns" :key="i" bottom>
-        <template v-slot:activator="{ on }">
-          <v-btn
-            v-on="on"
-            class="mr-1"
-            small
-            icon
-            :href="social_btn.url"
-            target="_blank"
-          >
-            <v-img
-              :src="require(`@/assets/img/social/${social_btn.icon}`)"
-              max-height="28"
-              max-width="28"
-            />
-          </v-btn>
-        </template>
-        <span v-text="social_btn.name" />
-      </v-tooltip>
-    </v-app-bar>
-
-    <v-main>
-      <v-container fluid>
-        <v-row>
-          <v-col
-            cols="12"
-            class="full-height full-bg"
-            :style="`background-image: url('${require('@/assets/img/bg/1.jpg')}');`"
-          >
-            HOLA
-          </v-col>
-          <v-col cols="12">
-            text
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            fin test
-          </v-col>
-        </v-row>
-      </v-container>
-    </v-main>
-
-    <v-footer app color="black" dark padless>
-      <v-row dense>
-        <v-col cols="12" class="text-center">
-          <small
-            v-text="`Copyright © ${new Date().getFullYear()}`"
-            class="pr-1"
-          />
-        </v-col>
-      </v-row>
-    </v-footer> -->
   </v-app>
 </template>
 
@@ -381,6 +255,9 @@ export default {
   data() {
     return {
       drawer: false,
+      screenMobile: false,
+      screenWidth: 0,
+      screenHeight: 0,
       items: [
         {
           title: "Principal",
@@ -421,36 +298,114 @@ export default {
           url: "https://wa.me/524611292699?text=Hola,%20somos%20Licenciado%20Punk!!!,%20¿qué%20podemos%20hacer%20por%20ti?",
         },
       ],
+      members: [
+        {
+          url: "img/member/1.jpg",
+          name: "Paco Greenwood",
+          instrument: "Gtr. Eléctrica | Voz Coros",
+          mobile: true,
+        },
+        {
+          url: "img/member/2.jpg",
+          name: "Chris Villa",
+          instrument: "Batería",
+          mobile: true,
+        },
+        {
+          url: "img/member/3.jpg",
+          name: "Said Ramos",
+          instrument: "Gtr. Eléctrica",
+          mobile: true,
+        },
+        {
+          url: "img/member/4.jpg",
+          name: "Dann Lara",
+          instrument: "Voz Principal",
+          mobile: true,
+        },
+        {
+          url: "img/member/5.jpg",
+          name: null,
+          instrument: null,
+          mobile: false,
+        },
+        {
+          url: "img/member/6.jpg",
+          name: "Sam Moretto",
+          instrument: "Bajo Eléctrico | Voz Coros",
+          mobile: true,
+        },
+      ],
+      shows: [
+        {
+          name: "Quéretaro, Qro.",
+          date: "2023-05-19",
+          hour: "22:00",
+          url: "img/shows/4.jpg",
+          amount: 100,
+          location: "https://goo.gl/maps/MjoDJmMvCD9G8YwH6",
+          details: null,
+          tickets: null,
+        },
+        {
+          name: "Quéretaro, Qro.",
+          date: "2023-04-22",
+          hour: "21:00",
+          url: "img/shows/3.jpg",
+          amount: 100,
+          location: "https://goo.gl/maps/MjoDJmMvCD9G8YwH6",
+          details: null,
+          tickets: null,
+        },
+        {
+          name: "Celaya, Gto.",
+          date: "2023-04-01",
+          hour: "22:00",
+          url: "img/shows/2.jpg",
+          amount: 100,
+          location: "https://goo.gl/maps/MjoDJmMvCD9G8YwH6",
+          details: null,
+          tickets: null,
+        },
+        {
+          name: "Celaya, Gto.",
+          date: "2023-02-25",
+          hour: "20:30",
+          url: "img/shows/1.jpg",
+          amount: 100,
+          location: "https://goo.gl/maps/MjoDJmMvCD9G8YwH6",
+          details: null,
+          tickets: null,
+        },
+      ],
     };
   },
+
   methods: {
     isMobile: function () {
-      var check = false;
-      (function (a) {
-        if (
-          /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(
-            a
-          ) ||
-          /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(
-            a.substr(0, 4)
-          )
-        )
-          check = true;
-      })(navigator.userAgent || navigator.vendor || window.opera);
-      return check;
+      const width = window.innerWidth;
+      const height = window.innerHeight;
+      return { width: width, height: height, mobile: width < 960 };
     },
+    onResize() {
+      this.screenMobile = this.isMobile().mobile;
+      this.screenWidth = this.isMobile().width;
+      this.screenHeight = this.isMobile().height;
+    },
+    numberFormat(v) {
+      return Number(parseFloat(v).toFixed(2)).toLocaleString("en-US", {
+        style: "currency",
+        currency: "USD",
+      });
+    },
+  },
+
+  mounted() {
+    this.onResize();
+
+    window.onresize = () => {
+      this.onResize();
+    };
   },
 };
 </script>
-
-<style>
-.full-height {
-  height: 100vh;
-}
-.full-bg {
-  background-repeat: no-repeat;
-  background-position: center center;
-  background-attachment: fixed;
-  background-size: cover;
-}
-</style>
